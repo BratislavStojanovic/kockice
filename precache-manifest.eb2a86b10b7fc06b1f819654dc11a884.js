4808a0d1145c9261d2ebfb2f84dab286166f160b
Bratislav Stojanovic <bata.stojan.83@gmail.com>
2019-06-17 21:54:12 +0200

    Updates

---
+++ precache-manifest.eb2a86b10b7fc06b1f819654dc11a884.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672fcb334c9568ac9f398e774d295f17",
+    "url": "/kockice/index.html"
+  },
+  {
+    "revision": "689cf1b84add08f08f84",
+    "url": "/kockice/static/css/main.f32157e6.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/kockice/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "689cf1b84add08f08f84",
+    "url": "/kockice/static/js/main.7c56e895.chunk.js"
+  },
+  {
+    "revision": "a16ea024c67014019521",
+    "url": "/kockice/static/js/runtime~main.9664989b.js"
+  }
+]);
